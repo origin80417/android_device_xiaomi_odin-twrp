a4935dd1529813cd0b4ec3fcbae00b34ee745bab
origin80417 <75294347+origin80417@users.noreply.github.com>
2022-03-04 09:35:03 -0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -9,12 +9,13 @@
 #
 # As much as possible, those components should be built unconditionally,
 # with device-specific names to avoid collisions, to avoid device-specific
+
 # bitrot and build breakages. Building a component unconditionally does
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),odinx)
+ifeq ($(TARGET_DEVICE),odin)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
